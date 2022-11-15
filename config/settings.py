249e7f991118a973dc26ea2ba9465506887bc995
RETBOT <robertoesquiveltr16@gmail.com>
2022-11-15 15:01:35 -0600

    heroku

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     # aplicaciones de terceros
-    'crispy_forms',
      # aplicaciones locales
     'blog',
     'publicaciones',
